ed0727c9f2c1230429be1e6cc30979c73090bf26
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-09-11 00:56:19 +0530

    Got to know type hunting - but it dont produce error

--- pydantic/1_pydantic_why.py
+++ pydantic/1_pydantic_why.py
@@ -1,8 +1,17 @@
-def insert_patient_data(name,age):
+# def insert_patient_data(name:,age):
+#     print(name)
+#     print(age)
+#     print('inserted into database')
+    
+# insert_patient_data('Sarthak','twenty')
+
+#Here consider  a scenario that there ia senior developer and junior developer ok so considering now we go in deep there ..I have decelread one function passing age and name ok but as a senior developer i know thta the data type of age i int but junior developer dont known so he has written 'Twenty' so to solve this we need pydantic
+
+#Type-valaidation nhi ho raha -python ki in general problem...dynmaikc typing nhi hoti
+
+def insert_patient_data(name:str,age:int):
     print(name)
     print(age)
     print('inserted into database')
     
 insert_patient_data('Sarthak','twenty')
-
-#Here consider  a scenario that there ia senior developer and junior developer ok so considering now we go in deep there ..I have decelread one function passing age and name ok but as a senior developer i know thta the data type of age i int but junior developer dont known so he has written 'Twenty' so to solve this we need pydantic
