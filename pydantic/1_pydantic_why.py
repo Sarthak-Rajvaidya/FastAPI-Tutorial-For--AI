c9aed68f2c51a0aa98949a356e88b9ebf9f71f0e
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-09-11 00:53:29 +0530

    Understood scenario to work on

--- pydantic/1_pydantic_why.py
+++ pydantic/1_pydantic_why.py
@@ -5,4 +5,4 @@ def insert_patient_data(name,age):
     
 insert_patient_data('Sarthak','twenty')
 
- 
+#Here consider  a scenario that there ia senior developer and junior developer ok so considering now we go in deep there ..I have decelread one function passing age and name ok but as a senior developer i know thta the data type of age i int but junior developer dont known so he has written 'Twenty' so to solve this we need pydantic
