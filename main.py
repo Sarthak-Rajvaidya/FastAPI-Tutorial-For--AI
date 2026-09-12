90313e37a65fb79a1bfcd8093eeb2fbd1dc11c7c
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-09-13 01:42:58 +0530

    @nd Pydanytic model for update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI,Path,HTTPException,Query
 from fastapi.responses import JSONResponse
 
 from pydantic import BaseModel,Field,computed_field
-from typing import Annotated,Literal
+from typing import Annotated,Literal,Optional
 
 import json
 
@@ -39,6 +39,16 @@ class Patient(BaseModel):
             return 'Obese'
     
     
+    
+#2nd pydantic model for update
+
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
     
     
 
