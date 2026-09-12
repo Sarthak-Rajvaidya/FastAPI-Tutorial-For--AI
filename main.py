dcc27686e996dcd2104057273ece70b5feee4b63
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-09-13 03:01:46 +0530

    Updated patient info

--- main.py
+++ main.py
@@ -153,3 +153,9 @@ def update_patient(patient_id:str,patient_update:PatientUpdate):
     if patient_id not in data:
         raise HTTPException(status_code = 404,detail ='Patient not found')
     
+    existing_patient_info = data[patient_id]
+    
+    updated_patient_info =patient_update.model_dump(exclude_unset = True)
+    
+    
+    
